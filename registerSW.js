b656ab0634ecf566202342c50b320347b6327099
Чиканчи Владислав <mr.vlad-2005@yandex.ru>
2025-11-27 20:21:45 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Assessment-of-the-effectiveness-of-building-thermal-insulation-Frontend/sw.js', { scope: '/Assessment-of-the-effectiveness-of-building-thermal-insulation-Frontend/' })})}
